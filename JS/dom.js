d71acff18fe5a396a0371ca9be6fa31f2da026fb
Federico Correa <fede.correa8@gmail.com>
2022-08-20 03:37:52 -0300

    images prac

--- JS/dom.js
+++ JS/dom.js
@@ -36,18 +36,18 @@ window.onload = iniciar();
 
 let catalogo = new Array();
 
-catalogo.push(new Productos(1,"BEBIDAS", "GASEOSA", "COCA-COLA", "12 x 2,25 LT", 2500, true, "../img/cocacola225.jpg"));
-catalogo.push(new Productos(2,"BEBIDAS", "GASEOSA", "COCA-COLA", "12 x 1,50 LT", 1800, true, "../img/coca15a.webp"));
-catalogo.push(new Productos(3,"BEBIDAS", "CERVEZA", "QUILMES", "12 x 1 LT", 2400, false, "../img/quilmes.jpg"));
-catalogo.push(new Productos(4,"BEBIDAS", "CERVEZA", "AMSTEL", "12 x 1 LT", 2000, true,"../img/amstel.webp" ));
-catalogo.push(new Productos(5,"BEBIDAS", "AGUA", "GLACIAR", "12 x 2LT", 1500, false, "../img/glaciar.webp"));
-catalogo.push(new Productos(6,"FIAMBRES", "QUESOS", "SANCOR", "BARRA x 5 KG", 5300, true, "../img/sancor.jpg"));
-catalogo.push(new Productos(7,"FIAMBRES", "JAMONES", "PALADINI", "PIEZA x 4 KG", 6200, true, "../img/jamonpaladini.png"));
-catalogo.push(new Productos(8,"FIAMBRES", "SALAMES", "CALCHAQUI", "PIEZA x 3 KG", 4800, true, "../img/salame.jpg"));
-catalogo.push(new Productos(9,"FIAMBRES", "QUESOS", "SERENISIMA", "BARRA x 5 KG", 5500, false, "../img/serenisima.jpg"));
-catalogo.push(new Productos(10,"FIAMBRE", "MORTADELA", "PALADINI", "PIEZA X 5 KG", 4300, true, "../img/mortadela.jpg"));
-catalogo.push(new Productos(11,"FIAMBRES", "FIAMBRIN", "SERENISIMA", "BARRA X 4 KG", 6200, true, "../img/fiambrin2.jpg"));
-catalogo.push(new Productos(12,"BEBIDAS", "VINO", "QUARA", "6 X 750 ML", 2520, true, "../img/quara.jpg"));
+catalogo.push(new Productos(1,"BEBIDAS", "GASEOSA", "COCA-COLA", "12 x 2,25 LT", 2500, true, "../Javascript/img/cocacola225.jpg"));
+catalogo.push(new Productos(2,"BEBIDAS", "GASEOSA", "COCA-COLA", "12 x 1,50 LT", 1800, true, "../Javascript/img/coca15a.webp"));
+catalogo.push(new Productos(3,"BEBIDAS", "CERVEZA", "QUILMES", "12 x 1 LT", 2400, false, "../Javascript/img/quilmes.jpg"));
+catalogo.push(new Productos(4,"BEBIDAS", "CERVEZA", "AMSTEL", "12 x 1 LT", 2000, true,"../Javascript/img/amstel.webp" ));
+catalogo.push(new Productos(5,"BEBIDAS", "AGUA", "GLACIAR", "12 x 2LT", 1500, false, "../Javascript/img/glaciar.webp"));
+catalogo.push(new Productos(6,"FIAMBRES", "QUESOS", "SANCOR", "BARRA x 5 KG", 5300, true, "../Javascript/img/sancor.jpg"));
+catalogo.push(new Productos(7,"FIAMBRES", "JAMONES", "PALADINI", "PIEZA x 4 KG", 6200, true, "../Javascript/img/jamonpaladini.png"));
+catalogo.push(new Productos(8,"FIAMBRES", "SALAMES", "CALCHAQUI", "PIEZA x 3 KG", 4800, true, "../Javascript/img/salame.jpg"));
+catalogo.push(new Productos(9,"FIAMBRES", "QUESOS", "SERENISIMA", "BARRA x 5 KG", 5500, false, "../Javascript/img/serenisima.jpg"));
+catalogo.push(new Productos(10,"FIAMBRE", "MORTADELA", "PALADINI", "PIEZA X 5 KG", 4300, true, "../Javascript/img/mortadela.jpg"));
+catalogo.push(new Productos(11,"FIAMBRES", "FIAMBRIN", "SERENISIMA", "BARRA X 4 KG", 6200, true, "../Javascript/img/fiambrin2.jpg"));
+catalogo.push(new Productos(12,"BEBIDAS", "VINO", "QUARA", "6 X 750 ML", 2520, true, "../Javascript/img/quara.jpg"));
 
 
 
@@ -130,3 +130,4 @@ function clickAgregar(){
     crearCaja(input);
 
 }
+
